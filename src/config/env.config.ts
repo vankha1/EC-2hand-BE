591e53a52213e28b1f53791cb959235ae1196ee8
vankha1 <vovankha2003@gmail.com>
2024-12-13 15:09:39 +0700

    fix: mongodb variable

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,6 @@
 export default () => ({
   port: parseInt(process.env.PORT),
-  database: process.env.DATABASE_URI,
+  database: process.env.MONGODB_URI,
   secret: process.env.SECRET,
   expired: process.env.EXPIRED,
   // client_host: process.env.CLIENT_HOST,
